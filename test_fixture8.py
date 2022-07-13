4b94df419cb72c2b0eb2fe16288ee22755694502
Hvosten1 <dogrer@gmail.com>
2022-07-13 13:34:03 +0300

    Added strict

--- test_fixture8.py
+++ test_fixture8.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
@@ -16,12 +15,16 @@ def browser():
 
 class TestMainPage1():
 
-    @pytest.mark.smoke
+    
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "button.favorite")
