c57c499dcd68b7dd2bcec89013d45fa754545b5d
Hvosten1 <dogrer@gmail.com>
2022-07-18 17:57:20 +0300

    Added fix

---
+++ test_fixture7.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+answer = math.log(int(time.time()))
+links = [
+'https://stepik.org/lesson/236895/step/1',
+'https://stepik.org/lesson/236896/step/1',
+'https://stepik.org/lesson/236897/step/1',
+'https://stepik.org/lesson/236898/step/1',
+'https://stepik.org/lesson/236899/step/1',
+'https://stepik.org/lesson/236903/step/1',
+'https://stepik.org/lesson/236904/step/1',
+'https://stepik.org/lesson/236905/step/1'
+]
+
+@pytest.mark.parametrize('link', links)
+class TestAnswer:
+    def  test_open_and_answer(self, link):
+        browser = webdriver.Chrome()
+
+
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
